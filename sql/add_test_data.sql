b86db127d4c8e3b1472dbec9101423086934e21e
Riku Leppänen <riku.m.leppanen@helsinki.fi>
2017-07-31 11:58:19 +0300

    DatabaseStarted

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,12 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Guru(name, email, admin_rights,password) VALUES ('Matti Mansikka', 'matti','TRUE','1234');
+INSERT INTO Guru(name, email, admin_rights,password) VALUES ('Raija Ratikka', 'raija','FALSE','4321');
+INSERT INTO Query(tstz,guru) VALUES (NOW(), (SELECT id FROM Guru WHERE name = 'Matti Mansikka'));
+INSERT INTO Customer(name, tstz) VALUES ('Osakeyhtiö AB', NOW());
+INSERT INTO Customer(name, email, address, number, tstz) VALUES ('Ville','ak@ak.fi','Lopeentie','0101111', NOW());
+INSERT INTO QueryCustomer(query, customer) VALUES ((SELECT id FROM Query WHERE guru = '1'),(SELECT id FROM Customer WHERE name = 'Osakeyhtiö AB'));
+INSERT INTO Consent(label) VALUES ('Puhelin');
+INSERT INTO Consent(label) VALUES ('Sähköposti');
+INSERT INTO Consent(label) VALUES ('Kirje');
+INSERT INTO CustomerConsent(customer, consent) VALUES ((SELECT id FROM Customer WHERE name = 'Osakeyhtiö AB'),(SELECT id FROM Consent WHERE label = 'Sähköposti'));
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,33 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Guru(
+    id SERIAL PRIMARY KEY, 
+    name varchar(60) NOT NULL, 
+    email varchar(40) NOT NULL, 
+    admin_rights boolean default FALSE,
+    password varchar(50) NOT NULL);
+
+CREATE TABLE Query(
+    id SERIAL PRIMARY KEY,
+    tstz TIMESTAMPTZ, 
+    guru INTEGER REFERENCES Guru(id));
+
+CREATE TABLE Customer(
+    id SERIAL PRIMARY KEY, 
+    name varchar(60) NOT NULL, 
+    email varchar(40),
+    address varchar(120),
+    number varchar(20),
+    tstz TIMESTAMPTZ);
+
+CREATE TABLE QueryCustomer(
+    id SERIAL PRIMARY KEY, 
+    query INTEGER REFERENCES Query(id), 
+    customer integer REFERENCES Customer(id));
+
+CREATE TABLE Consent(
+    id SERIAL PRIMARY KEY, 
+    label varchar(50) NOT NULL);
+
+CREATE TABLE CustomerConsent(
+    id SERIAL PRIMARY KEY, 
+    customer INTEGER REFERENCES Customer(id), 
+    consent INTEGER  REFERENCES Consent(id));
