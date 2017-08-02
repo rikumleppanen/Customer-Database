f6a075e2c7d94f457060db576aa2a78d6fcd1041
Riku Leppänen <riku.m.leppanen@helsinki.fi>
2017-08-02 11:54:15 +0300

    AddedFirstPages

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,12 +4,23 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-        echo 'Tämä on etusivu!';
+        View::make('login.html');
     }
 
-    public static function sandbox() {
-        // Testaa koodiasi täällä
-        View::make('helloworld.html');
+    public static function guru_change() {
+        View::make('modifyCustomer.html');
+    }
+
+    public static function guru_list() {
+        View::make('browseCustomers.html');
+    }
+
+    public static function guru_query() {
+        View::make('makeAQuery.html');
+    }
+
+    public static function quru_login() {
+        View::make('login.html');
     }
 
 }
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,26 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/drafts/qlist', function() {
+    HelloWorldController::guru_list();
+});
+
+$routes->get('/drafts/qchange', function() {
+    HelloWorldController::guru_change();
+});
+
+$routes->get('/drafts/login', function() {
+    HelloWorldController::guru_login();
+});
+
+$routes->get('/drafts/query', function() {
+    HelloWorldController::guru_query();
+});
+
